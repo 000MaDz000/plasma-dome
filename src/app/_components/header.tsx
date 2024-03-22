f56db9c77e51b3d8abaca712bd363bc150bde4fd
mostafa mohammed <000madz000@gmail.com>
2024-03-22 04:09:00 +0200

    removing the background of the header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Header({ children }: { children?: React.ReactNode }) {
     return (
-        <header className="flex justify-between items-center py-3 px-14 bg-gradient-to-l from-slate-700 via-sky-950 to-gray-800">
-            <Image src="/logo.png" alt="" width={80} height={80} className="bg-gray-800 rounded-full" />
+        // bg-gradient-to-l from-slate-700 via-sky-950 to-gray-800
+        <header className="flex justify-between items-center py-3 px-14 bg-transparent">
+            <Image src="/logo.png" alt="" width={80} height={80} className="rounded-full bg-blend-darken" />
 
             <div className="">
                 {children}
