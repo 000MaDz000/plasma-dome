8f1f9d979c9e4995d572d207cb3f9762fc2cc465
mostafa mohammed <000madz000@gmail.com>
2024-07-06 08:39:06 +0300

    adding search query when search

--- src/app/_components/store-header.components.tsx
+++ src/app/_components/store-header.components.tsx
@@ -4,9 +4,24 @@ import { BiSearch } from "react-icons/bi";
 import { FaMotorcycle } from "react-icons/fa";
 import StoreCartIcon from "./store-cart-icon";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { KeyboardEventHandler, useRef } from "react";
 
 export default function StoreHeaderComponents() {
     const t = useTranslations("Store.header")
+    const input = useRef<HTMLInputElement>(null);
+    const router = useRouter();
+
+    const handleSearch: KeyboardEventHandler<HTMLInputElement> = (e) => {
+        if (!input.current || !input.current.value || e.key !== "Enter") return;
+
+        const params = new URLSearchParams();
+        params.set("search", input.current.value);
+        console.log(params);
+
+        router.push("/products?" + params.toString());
+    }
+
     return (
         <>
             <StoreCartIcon />
@@ -24,7 +39,9 @@ export default function StoreHeaderComponents() {
                         <BiSearch />
                     </InputAdornment>
                 )}
+                inputRef={input}
                 placeholder={t("searchPlaceholder")}
+                onKeyDown={handleSearch}
             />
         </>
     )
