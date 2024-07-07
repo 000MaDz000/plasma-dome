63f0e0dc203c333b7f4af8a74d70490abc5be7ec
mostafa mohammed <000madz000@gmail.com>
2024-07-07 14:06:23 +0300

    init the product page fix the wrong redirect error in the product card make the store header redirects to the home page on click

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,15 +1,18 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header({ children, className }: { children?: React.ReactNode, className?: string }) {
     const t = useTranslations("Index");
     return (
         // bg-gradient-to-l from-slate-700 via-sky-950 to-gray-800
         <header className={"flex justify-between items-center py-3 px-4 sm:px-14 bg-transparent sticky top-0 left-0 bg-gradient-to-r from-gray-700 to-gray-700 via-gray-600 " + (className || "")}>
-            <div className="flex gap-3 items-center">
-                <Image src="/logo.png" alt="" width={80} height={80} className="rounded-full bg-blend-darken" />
-                <h1>{t("title")}</h1>
-            </div>
+            <Link href="/store">
+                <div className="flex gap-3 items-center">
+                    <Image src="/logo.png" alt="" width={80} height={80} className="rounded-full bg-blend-darken" />
+                    <h1>{t("title")}</h1>
+                </div>
+            </Link>
 
             <div className="">
                 {children}
--- src/app/_components/product-card.tsx
+++ src/app/_components/product-card.tsx
@@ -1,6 +1,6 @@
 import { IImage } from "@/models/image";
 import { Box, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
-import { getTranslations } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 import AddToCartButton from "./add-to-cart-button";
 import Link from "next/link";
 import { ICartProduct } from "../_actions/get-cart-data";
@@ -8,10 +8,10 @@ import { ICartProduct } from "../_actions/get-cart-data";
 
 export default async function ProductCard({ isAddedToCart, product }: { isAddedToCart?: boolean, product: Omit<ICartProduct, "quantity"> }) {
     const t = await getTranslations("Store");
-
+    const locale = await getLocale();
     return (
         <Card className="shadow-lg hover:border-gray-200 border-transparent border transition-colors flex justify-between flex-col cursor-pointer min-w-56">
-            <Link href={"/store/products/" + product._id}>
+            <Link href={"/" + locale + "/products/" + product._id} >
                 <Box className="flex flex-col">
                     <CardMedia>
                         <img src={((product.images[0] as unknown as IImage).relativeUrl.toString() || "") as string} alt="" className="w-full h-40" />
