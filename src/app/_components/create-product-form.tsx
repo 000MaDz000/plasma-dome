f831b21c6c6dd341e51e9ff861a665684364ee90
mostafa mohammed <000madz000@gmail.com>
2024-07-08 23:14:03 +0300

    update create-product-form Bad UI

--- src/app/_components/create-product-form.tsx
+++ src/app/_components/create-product-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, Button, FormControl, Input, InputLabel, MenuItem, Select } from "@mui/material";
+import { Box, Button, FormControl, Input, InputLabel, MenuItem, Select, TextareaAutosize } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { ChangeEvent, useRef, useState } from "react";
 import { FaUpload } from "react-icons/fa6";
@@ -32,9 +32,8 @@ export default function CreateProductForm() {
     return (
         <form method="post" encType="multipart/form-data" action="/api/products" className="grid md:grid-cols-2 gap-3">
             <Input name="name" placeholder={t("name")} className="border p-3" />
-            <Input name="description" placeholder={t("description")} className="border p-3" />
             <Input name="price" placeholder={t("price")} className="border p-3" />
-            <Input name="categories" placeholder={t("categories")} className="border p-3" />
+            <Input name="categories" placeholder={t("categories")} className="border p-3 col-span-2" />
             <input id="create-product-file-upload" type="file" name="images" placeholder={t("images")} className="hidden" ref={fileRef} onChange={onUploadImg} />
 
             <FormControl fullWidth>
@@ -49,6 +48,9 @@ export default function CreateProductForm() {
                 <InputLabel id="create-product-shotype-level">{t("show type level")}</InputLabel>
                 <Input defaultValue={"1"} name="showTypeLevel" />
             </FormControl>
+
+            <TextareaAutosize name="description" placeholder={t("description")} className="border p-3 col-span-2 row-span3 min-h-60" />
+
 
             {
                 uploaded && (
