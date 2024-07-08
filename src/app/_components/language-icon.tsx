2ebcdeda2bfff22f91926f47708785e8f94bd91d
mostafa mohammed <000madz000@gmail.com>
2024-07-08 22:18:23 +0300

    fix error the translation function didn't change when change the locale this commit fix this error by refresh the page and adding the loading circle when transation is pending

--- src/app/_components/language-icon.tsx
+++ src/app/_components/language-icon.tsx
@@ -1,35 +1,49 @@
 'use client';
 
-import { Box, Button, IconButton, Input, ListItemIcon, Menu, MenuItem, Select } from "@mui/material";
+import { Box, CircularProgress, IconButton, Menu, MenuItem, } from "@mui/material";
 import { useLocale, useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
-import { useRef, useState } from "react";
-import { FaArrowDown, FaLanguage } from "react-icons/fa";
+import { usePathname, useRouter } from "next/navigation";
+import { useRef, useState, useTransition } from "react";
+import { FaLanguage } from "react-icons/fa";
 
 export default function LanguageIcon() {
     const locale = useLocale();
+    const pathname = usePathname();
     const router = useRouter();
     const t = useTranslations("Locales");
-
+    const [isPending, startTransition] = useTransition();
     const [menu, setMenu] = useState(false);
-    const ref = useRef<any>(null);
+    const ref = useRef<HTMLElement>(null);
 
     const changeLanguage = (locale: string) => {
-        const currentPathWithoutLocale = location.pathname.slice(3);
-        router.replace("/" + locale + currentPathWithoutLocale);
+        startTransition(() => {
+            router.replace("/" + locale + pathname.slice(3), { locale } as any);
+            router.refresh();
+        });
+
+
+        setMenu(false);
     }
 
     return (
         <Box className="flex items-center justify-center">
+            {isPending ? (
+                <>
+                    <CircularProgress size={"1rem"} />
+                </>
+            ) : (
+                <IconButton onClick={() => setMenu(true)} ref={ref as any}>
+                    <FaLanguage />
+                </IconButton>
+            )
+            }
 
-            <IconButton onClick={() => setMenu(true)} ref={ref}>
-                <FaLanguage />
-            </IconButton>
-
-            <Menu open={menu} anchorEl={() => ref.current} onClose={() => setMenu(false)} onChange={(e) => console.log(e)}>
-                <MenuItem onClick={() => changeLanguage("ar")} disabled={locale === "ar"}>{t("ar")}</MenuItem>
-                <MenuItem onClick={() => changeLanguage("en")} disabled={locale === "en"}>{t("en")}</MenuItem>
-            </Menu>
+            {ref.current && (
+                <Menu open={menu} anchorEl={() => ref.current as any} onClose={() => setMenu(false)} onChange={(e) => console.log(e)}>
+                    <MenuItem onClick={() => changeLanguage("ar")} disabled={locale === "ar"}>{t("ar")}</MenuItem>
+                    <MenuItem onClick={() => changeLanguage("en")} disabled={locale === "en"}>{t("en")}</MenuItem>
+                </Menu>
+            )}
         </Box>
     )
 }
