4f71785e9cfc51328d0b3126910072e73c4b3ec5
mostafa mohammed <000madz000@gmail.com>
2024-07-09 21:16:26 +0300

    adding whatsapp link to the footer

--- src/app/_components/store-footer.tsx
+++ src/app/_components/store-footer.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function StoreFooter() {
     return (
 
-        <Box className="min-h-80 bg-gray-200 mt-9 grow">
-
+        <Box className="min-h-80 bg-gray-200 mt-9 grow p-7">
+            <Typography variant="h5">Copyright by MaDz</Typography>
+            <Typography variant="h5">contact: <a href="https://wa.me/01155029839" className="text-sky-600">Whatsapp</a></Typography>
         </Box>
     )
 }
