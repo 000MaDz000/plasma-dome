de0335fc76e4e55d97d2e316370b17e10f0a1206
mostafa mohammed <000madz000@gmail.com>
2024-07-11 23:25:02 +0300

    remove landing page components

--- src/app/_components/landing-header-menu-trigger.tsx
+++
@@ -1,8 +0,0 @@
-import { IoIosMenu } from "react-icons/io"
-export default function LandingHeaderMenuTrigger() {
-    return (
-        <div>
-            <IoIosMenu size={"1.75rem"} className="text-white" />
-        </div>
-    )
-}
