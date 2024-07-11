0df1067d4b553d62e6a3a50edfe30d697d6cbcf9
mostafa mohammed <000madz000@gmail.com>
2024-07-11 23:16:23 +0300

    delete signup page

--- src/app/[locale]/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from "@/app/_components/signup-form";
-
-export default function SignupPage() {
-    return (
-        <div className="p-4 bg-white mx-auto rounded shadow sm:w-1/2 md:w-[43vw]">
-            <SignupForm />
-        </div>
-    )
-}
